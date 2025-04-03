d2801797ee827729257f8672f7fc9ef8c8a7421b
YoonGangHoon <yghun021007@naver.com>
2025-04-04 00:50:45 +0900

    Chore: 파일 경로 재설정

--- data_fetch/store_fetcher.py
+++ data_fetch/store_fetcher.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 import requests
 from config import config
-import geocode_converter
+from . import geocode_converter
 
 def load_store_data():
     with open(STORE_JSON_PATH, "r", encoding="utf-8") as f:
