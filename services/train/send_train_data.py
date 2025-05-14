c18021f0fbe2277d06e623da455c79a73e0875ee
MOJAN <sjs732@naver.com>
2025-05-15 02:05:08 +0900

    Feat: train 스케쥴러 추가

--- services/train/send_train_data.py
+++ services/train/send_train_data.py
@@ -12,3 +12,19 @@ def send_prophet_data():
 def send_xgboost_data():
     category = sys._getframe().f_code.co_name.split('_')[1]
     train_utils.integration(category)
+
+if __name__ == '__main__':
+    argv = sys.argv
+    if len(argv) != 2 or (argv[1] != "all" and argv[1] != 'cluster' and argv[1] != 'prophet' and argv[1] != 'xgboost'):
+        print(f"usage: python {argv[0]} (all | cluster | prophet | xgboost)")
+    else:
+        if argv[1] == "all":
+            send_cluster_data()
+            send_prophet_data()
+            send_xgboost_data()
+        if argv[1] == "cluster":
+            send_cluster_data()
+        if argv[1] == "prophet":
+            send_prophet_data()
+        if argv[1] == "xgboost":
+            send_xgboost_data()
